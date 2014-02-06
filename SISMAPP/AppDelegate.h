9c86348010e89fc7aedfc46e85ee71e79bce80ff
AxelArd <ardoin.axel@gmail.com>
2014-02-06 14:16:13 +0100

    Initial Commit

---
+++ SISMAPP/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SISMAPP
+//
+//  Created by Axel on 06/02/14.
+//  Copyright (c) 2014 axelardoin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SISMAPP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SISMAPP
+//
+//  Created by Axel on 06/02/14.
+//  Copyright (c) 2014 axelardoin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
